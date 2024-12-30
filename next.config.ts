e1df0605e3100b352c832373e49374bbc46191b9
rougegez <rougetgc@gmail.com>
2024-12-30 11:53:45 +0800

    Prepare for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'images.unsplash.com',
-      // add other domains here if needed
-    ],
+    domains: ['images.unsplash.com'],
+  },
+  // Add this if you're using TypeScript
+  typescript: {
+    // !! WARN !!
+    // This is for deployment only, remove in development
+    // ignoreBuildErrors: true,
   },
 }
 
