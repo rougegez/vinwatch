f96b93a775d8eb76411cff87e57ce4ded3c2357e
rougegez <rougetgc@gmail.com>
2024-12-30 13:04:40 +0800

    new img

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
   return (
     <section className="relative h-[600px] flex items-center">
       <Image
-        src="https://images.unsplash.com/photo-1509048191080-d2984bad6ae5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+        src="https://images.unsplash.com/photo-1511370235399-1802cae1d32f?q=80&w=2055&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Vintage watch collection"
         fill
         style={{ objectFit: 'cover' }}
