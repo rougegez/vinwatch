3aef4e4f91fc9bfdf8c32d7f03b687effe23a214
rougegez <rougetgc@gmail.com>
2024-12-30 12:07:21 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,11 @@ const nextConfig = {
     domains: ['images.unsplash.com'],
   },
   eslint: {
-    // Only use this if you want to completely disable ESLint during builds
     ignoreDuringBuilds: true,
   },
-  // Add this if you're using TypeScript
   typescript: {
-    // !! WARN !!
-    // This is for deployment only, remove in development
-    // ignoreBuildErrors: true,
-  },
+    ignoreBuildErrors: true,
+  }
 }
 
 module.exports = nextConfig
